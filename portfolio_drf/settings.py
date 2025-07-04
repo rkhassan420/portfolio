5e550d6180ecb149d0ca89270663603081186c58
Ali Hassan <mrkhan111243@gmail.com>
2025-07-04 12:52:56 +0500

    db_change

--- portfolio_drf/settings.py
+++ portfolio_drf/settings.py
@@ -89,13 +89,23 @@ WSGI_APPLICATION = 'portfolio_drf.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'OBFKOnEUFUZcXdMRJZejwhnpGNXPUtnC',
+        'HOST': 'nozomi.proxy.rlwy.net',
+        'PORT': '32759',
     }
 }
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 #     'default': {
 #         'ENGINE': 'django.db.backends.mysql',
 #         'NAME': 'portfolio',
