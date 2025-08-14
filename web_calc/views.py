bf7caf39225f0d6e79bb3ce8b222c55eb87983df
Ali Hassan <mrkhan111243@gmail.com>
2025-08-14 23:05:45 +0500

    Your commit message here

--- web_calc/views.py
+++ web_calc/views.py
@@ -1,10 +1,14 @@
+from django.views.decorators.csrf import csrf_protect
 from rest_framework import status
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializers import AgeSerializer
 
-
+@csrf_protect
+@permission_classes([AllowAny])
 class AgeCalculatorView(APIView):
     def post(self, request):
         serializer = AgeSerializer(data=request.data)
