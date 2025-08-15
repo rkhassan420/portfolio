97b5087288d274c0e02458a59b5aa7263a210617
Ali Hassan <mrkhan111243@gmail.com>
2025-08-15 15:55:52 +0500

    first commit

--- pass_generator/views.py
+++ pass_generator/views.py
@@ -1,10 +1,12 @@
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from .utils import generate_password, check_strength
 
 
 @api_view(['POST'])
+@permission_classes([AllowAny])
 def password_generator(request):
     try:
         length = int(request.data.get("length", 12))
@@ -24,6 +26,7 @@ def password_generator(request):
 
 
 @api_view(['POST'])
+@permission_classes([AllowAny])
 def password_strength(request):
     password = request.data.get("password", "")
     if not password:
